cbaefc0ead2998364fae7dc1e3800a8ac8d49682
adhiraj-ranjan <54994272+adhiraj-ranjan@users.noreply.github.com>
2019-10-24 14:37:30 +0530

    Add files via upload

---
+++ calcu-lator.py
@@ -0,0 +1,25 @@
+# Check Examples
+#created by Adhiraj_Ranjan
+
+#python 3.7.1
+print ("_________        .__               .__          __             ")       
+
+print ("\_   ___ \_____  |  |   ____  __ __|  | _____ _/  |_  ___________")
+print ("/    \  \/\__  \ |  | _/ ___\|  |  \  | \__  \\   __\/  _ \_  __ \ ")
+print ("\     \____/ __ \|  |_\  \___|  |  /  |__/ __ \|  | (  <_> )  | \/")
+print (" \______  (____  /____/\___  >____/|____(____  /__|  \____/|__|")
+print ("        \/     \/          \/                \/              ")
+
+a=int(input("ENTER THE FIRST DIGIT:"))
+b=int(input("ENTER THE SECOND DIGIT:"))
+print("\n\nThe sum of numbers is:",a+b)
+print("\nThe diffence of numbers is:",a-b)
+print("\nThe product of the numbers is:",a*b)
+print("\nThe cube of the first digit=",a**3)
+print("\nThe cube of the second number is ",b**3)
+print("\nNOW,")
+gt=int(input("The Number that i thought in my mind ranging from 1 to 20:"))
+if gt==10:
+    print("CONGRATS! You are right")
+else:
+    print("You are wrong")
